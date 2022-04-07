bb9ae2018cd8a0071779860aab88e18504529c7b
clctrulala <clctrulala@gmail.com>
2022-04-07 20:08:43 +0300

    implement

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,7 +1,9 @@
 package app;
 
 import app.config.AppConfig;
+import app.model.Animal;
 import app.model.AnimalsCage;
+import app.model.Dog;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -10,6 +12,7 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         for (int i = 0; i < 5; i++) {
             AnimalsCage bean =
                     applicationContext.getBean(AnimalsCage.class);
